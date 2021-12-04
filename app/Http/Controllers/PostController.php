25ff102300f5008146a37c40b2a41d52726a3c60
dojagooo <dojagooo@gmail.com>
2021-12-04 10:41:26 +0900

    08-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(Post $post)
+    public function show(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]); 
+        return view('posts/show')->with(['post' => $post]); 
     }
 }
 ?>
