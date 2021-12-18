8b7052501d0bdef35f82042e4cb7110c4b5ccd0c
dojagooo <dojagooo@gmail.com>
2021-12-18 14:55:53 +0900

    Complete basic carriculum

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use App\Http\Requests\PostRequest;
+use App\Category;
+
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+       // クライアントインスタンス生成
+        $client = new \GuzzleHttp\Client();
+
+        // GET通信するURL
+        $url = 'https://teratail.com/api/v1/questions';
+
+        // リクエスト送信と返却データの取得
+        // Bearerトークンにアクセストークンを指定して認証を行う
+        $response = $client->request(
+            'GET',
+            $url,
+            ['Bearer' => config('services.teratail.token')]
+        );
+        
+        // API通信で取得したデータはjson形式なので
+        // PHPファイルに対応した連想配列にデコードする
+        $questions = json_decode($response->getBody(), true);
+        
+        // index bladeに取得したデータを渡す
+        return view('posts/index')->with([
+            'posts' => $post->getPaginateByLimit(),
+            'questions' => $questions['questions'],
+        ]);
     }
     
     public function show(Post $post)
@@ -17,11 +41,11 @@ class PostController extends Controller
         return view('posts/show')->with(['post' => $post]);
     }
     
-    public function create()
+    public function create(Category $category)
     {
-        return view('posts/create'); 
+        return view('posts/create')->with(['categories' => $category->get()]);;
     }
-    
+        
     public function store(PostRequest $request, Post $post)
     {
         $input = $request['post'];
@@ -47,6 +71,7 @@ class PostController extends Controller
     $post->delete();
     return redirect('/');
     }
+    
 
 }
 
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -29,15 +29,7 @@
             </div>
             @endforeach
             
-           <div>
-                @foreach($questions as $question)
-                    <div>
-                        <a href="https://teratail.com/questions/{{ $question['id'] }}">
-                            {{ $question['title'] }}
-                        </a>
-                    </div>
-                @endforeach
-            </div>
+           
            
         </div>
         
